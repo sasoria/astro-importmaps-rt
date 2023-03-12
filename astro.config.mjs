6b9b62e103eb73b34aa8af50b1e66f751ac329e5
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-03-12 12:04:42 +0100

    Add example

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [
+    react(),
+    {
+      name: 'importmap-externals',
+      hooks: {
+        'astro:build:setup': ({ vite, target }) => {
+          if(target === 'client') {
+            vite.build.rollupOptions["external"] = ["react", "react-dom"];
+          }
+        }
+       }
+    }
+  ]
+});
---
+++ src/components/ReactComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactComponent = () => (
+  <h2>React component</h2>
+);
+
+export default ReactComponent;
